67459efed9eaad40773dd2d0aa4b151f3495a766
Tarun Tailor <tarun@crimecheck.in>
2024-02-21 23:59:16 +0530

    small log monitoring system on http server

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log("Request: ",  req);
-    res.write("Welcome to our home page!");
-    res.end();
+    const log = `${Date.now()}: ${req.url} New Req Received\n`;
+    fs.appendFile("log.txt", log, (error, data) => {
+        switch(req.url) {
+            case "/": res.end("HomePage"); break;
+            case "/about": res.end("About Page"); break;
+            default: res.end("404 Not Found");
+        }
+    });
 })
 
-server.listen(5000);
+server.listen(5000, () => {
+    console.log("Server listening on port 5000");
+});
