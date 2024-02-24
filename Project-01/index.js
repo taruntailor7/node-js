aaed9a142852c01d5317ef5f54c80a53ee64cff6
Tarun Tailor <tarun@crimecheck.in>
2024-02-25 00:07:07 +0530

    http status code

--- Project-01/index.js
+++ Project-01/index.js
@@ -30,7 +30,10 @@ app.get("/api/users", (req, res) => {
 
 app.post("/api/users", (req, res) => {
     const body = req.body;
-    console.log(body, ' :body'); 
+    console.log(body, ' :body');
+    if(!body || !body.email) {
+        return res.status(400).send({msg: "email is required"});
+    }
     users.push({...body, id:users.length+1});
     fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
         return res.json({status: "Success", id: users.length});
@@ -44,7 +47,7 @@ app
     // Always ass X to custom headers
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
-    return res.json(user);
+    return res.status(201).json(user);
 })
 .patch((req,res) => {
     const id = Number(req.params.id);
